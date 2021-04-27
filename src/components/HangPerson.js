a4dc3a465395f7eb9ce97e58bc611d9678399ae2
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-27 15:22:45 -0700

    render hangperson
    Co-authored-by: MorganJBradford <MorganJBradford@users.noreply.github.com>

--- src/components/HangPerson.js
+++ src/components/HangPerson.js
@@ -1,3 +1,15 @@
 import React, { Component } from "react";
 import "./../styles/HangPerson.css";
 import { randomWord } from './Words.js';
+
+export default class HangPerson extends Component {
+
+  render() {
+    return (
+      <>
+        <h1>HANGPERSON</h1>
+      </>
+    )
+  }
+}
+
