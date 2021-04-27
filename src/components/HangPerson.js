fbfb91b024e333096019d1e7a58e37c35218c04c
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-27 15:30:11 -0700

    add default props and constructor
    Co-authored-by: MorganJBradford <MorganJBradford@users.noreply.github.com>

--- src/components/HangPerson.js
+++ src/components/HangPerson.js
@@ -4,6 +4,19 @@ import { randomWord } from './Words.js';
 
 export default class HangPerson extends Component {
 
+  static defaultProps = {
+    maxWrong: 6,
+    images: [step0, step1, step3, step4, step5, step6]
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      mistake: 0,
+      guessed: new Set([]),
+      answer: randomWord()
+    }
+  }
   render() {
     return (
       <>
